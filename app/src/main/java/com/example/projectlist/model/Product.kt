60ef6056f89b346114cb64b5f42b4f5213be9c5d
Arthur Augusto <arthuraugusn@gmail.com>
2023-02-13 11:20:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projectlist/model/Product.kt
@@ -0,0 +1,8 @@
+package com.example.projectlist.model
+
+class Product {
+    val id : Long = 0
+    val productName: String = ""
+    val productPrice: Double = 0.0
+    val productDescription:String = ""
+}
